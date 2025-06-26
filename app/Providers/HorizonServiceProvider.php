65e0b4ba416521bb769298622a715df41f77260a
weijiajia <shadmatthew1025@gmail.com>
2025-06-26 22:08:07 +0800

    更新 HorizonServiceProvider 中的权限定义，使用类型提示确保用户对象的正确性，提升代码可读性和类型安全性。

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -27,7 +27,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function ($user) {
+        Gate::define('viewHorizon', function (User $user) {
             return $user->id === User::first()->id;
         });
     }
