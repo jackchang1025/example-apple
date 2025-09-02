28dd0d4578287ae475ce60be5170da21a5885d37
weijiajia <shadmatthew1025@gmail.com>
2025-09-02 21:29:44 +0800

    删除 start-container 脚本和 LoginDelegatesTest 测试文件，简化代码库。同时在 ExampleTest 中新增对 Phone 模型的实例化检查，提升代码可读性和测试覆盖率。

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Account;
+use App\Models\Phone;
+use Illuminate\Database\Eloquent\Collection;
 
 uses(RefreshDatabase::class);
 
 it('returns a successful response', function () {
     $response = $this->get('/');
 
+
+    expect(Phone::all())->toBeInstanceOf(Collection::class)->dump();
     $response->assertStatus(200);
 });
